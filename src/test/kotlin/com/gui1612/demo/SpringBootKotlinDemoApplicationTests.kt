7d8ad889e750d8e1000a3f61c49fe01da89595c9
Guilherme Almeida <me@gui1612.com>
2023-02-21 23:18:11 +0000

    feat: generates Spring Boot project with Spring Initializr

---
+++ src/test/kotlin/com/gui1612/demo/SpringBootKotlinDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gui1612.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
